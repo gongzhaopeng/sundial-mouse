9f48463c124146ccd31ab7396b709324d4ce2e36
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-08-15 11:36:41 +0800

    ...

--- src/main/java/cn/benbenedu/sundial/mouse/configuration/ResourceServerConfigurer.java
+++ src/main/java/cn/benbenedu/sundial/mouse/configuration/ResourceServerConfigurer.java
@@ -1,16 +1,10 @@
 package cn.benbenedu.sundial.mouse.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableResourceServer
@@ -29,22 +23,7 @@ public class ResourceServerConfigurer
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        http.cors();
-
         http.authorizeRequests()
                 .anyRequest().authenticated();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
