5550d865b2383e3435e0337e41d71227edbb7be9
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-09-11 11:06:55 +0800

    ...

--- src/main/java/cn/benbenedu/sundial/mouse/controller/OAuthMouseController.java
+++ src/main/java/cn/benbenedu/sundial/mouse/controller/OAuthMouseController.java
@@ -3,9 +3,12 @@ package cn.benbenedu.sundial.mouse.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/genesis/oauth")
@@ -16,9 +19,14 @@ public class OAuthMouseController {
     private OAuth2RestTemplate oAuth2RestTemplate;
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(OAuth2Authentication auth) {
 
         log.info("Assess Token: {}", oAuth2RestTemplate.getAccessToken());
+
+        final var userPrincipal = (Map) auth.getPrincipal();
+        log.info("User ID: {}", userPrincipal.get("id"));
+        log.info("User Name: {}", userPrincipal.get("name"));
+        log.info("User Nickname: {}", userPrincipal.get("nickname"));
 
         return "Hello, OAuth...";
     }
